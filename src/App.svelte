962cb8e1ab0684fd906855c295278edb3540b56a
NotteShock <lekruistz@gmail.com>
2024-07-04 10:47:32 +0000

    upd

--- src/App.svelte
+++ src/App.svelte
@@ -88,11 +88,23 @@
     <button class="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95">Save</button>
   </div>
 </div>
-<div class="flex items-center">
-    <input type="checkbox" id="custom-checkbox" class="appearance-none h-5 w-5 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200 dark:focus:ring-blue-800">
-    <label for="custom-checkbox" class="ml-2 text-gray-700 dark:text-gray-300">Custom Checkbox</label>
-    <div class="flex h-6 items-center">
-                <input id="offers" name="offers" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-              </div>
-</div>
+<div class="inline-flex items-center">
+  <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
+    <input type="checkbox"
+      class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+      id="check" />
+    <span
+      class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+        stroke="currentColor" stroke-width="1">
+        <path fill-rule="evenodd"
+          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+          clip-rule="evenodd"></path>
+      </svg>
+    </span>
+  </label>
+  <label class="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="check">
+    Remember Me
+  </label>
+</div> 
 </main>
