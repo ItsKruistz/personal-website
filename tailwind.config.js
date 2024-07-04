8d14b9b9d0d75859ae1900e093001f47df5679b8
NotteShock <lekruistz@gmail.com>
2024-07-04 03:03:19 +0000

    add

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  daisyui: {
+    themes: ["light", "dark"]
+  },
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
